3864bc1dc1808beddee027a2355045e304ca26da
JunhaDex <dk.junha.dev@gmail.com>
2023-03-03 16:02:07 +0900

    add i18n

--- src/commands/CalenderEvent.ts
+++ src/commands/CalenderEvent.ts
@@ -1,4 +1,4 @@
-import { SlashCommand } from '../structure';
+import {SlashCommand} from '../structure';
 import {
   ActionRowBuilder,
   AttachmentBuilder,
@@ -26,51 +26,14 @@ function commonEmbedBuilder(title: string): EmbedBuilder {
     .setThumbnail(`${cdn}/logo.webp`);
 }
 
-export const Ping: SlashCommand = {
-  data: new SlashCommandBuilder()
-    .setName('ping')
-    .setNameLocalizations({ ko: '서버핑' })
-    .setDescription('server ping')
-    .setDescriptionLocalizations({ ko: '서버 연결을 확인합니다' }),
-  execute: async (interaction: CommandInteraction) => {
-    const exampleEmbed = new EmbedBuilder()
-      .setTitle('Some title')
-      .setURL('https://discord.js.org/')
-      .setAuthor({
-        name: 'Some name',
-        iconURL: 'https://i.imgur.com/AfFp7pu.png',
-        url: 'https://discord.js.org',
-      })
-      .setDescription('Some description here')
-      .setThumbnail('https://i.imgur.com/AfFp7pu.png')
-      .addFields(
-        { name: 'Regular field title', value: 'Some value here' },
-        { name: '\u200B', value: '\u200B' },
-        { name: 'Inline field title', value: 'Some value here', inline: true },
-        { name: 'Inline field title', value: 'Some value here', inline: true }
-      )
-      .addFields({
-        name: 'Inline field title',
-        value: 'Some value here',
-        inline: true,
-      })
-      .setImage('https://i.imgur.com/AfFp7pu.png')
-      .setTimestamp()
-      .setFooter({
-        text: 'Some footer text here',
-        iconURL: 'https://i.imgur.com/AfFp7pu.png',
-      });
-    await interaction.reply({ embeds: [exampleEmbed], ephemeral: true });
-  },
-};
-
 export const getUpcomingEvents: SlashCommand = {
   data: new SlashCommandBuilder()
     .setName('cal-list')
+    .setNameLocalizations({ko: '일정목록'})
     .setDescription('show upcoming events')
-    .setDescriptionLocalizations({ ko: '다가오는 이벤트를 확인합니다' }),
+    .setDescriptionLocalizations({ko: '다가오는 이벤트를 확인합니다'}),
   execute: async (interaction: CommandInteraction) => {
-    await interaction.deferReply({ ephemeral: true });
+    await interaction.deferReply({ephemeral: true});
     const calendar = new GCalendarProvider();
     const events = await calendar.listEvents();
     let file: AttachmentBuilder | undefined;
@@ -98,44 +61,12 @@ export const getUpcomingEvents: SlashCommand = {
     });
   },
 };
-export const broadcastEvents: SlashCommand = {
-  data: new SlashCommandBuilder()
-    .setName('cal-alert')
-    .setDescription('alert channel upcoming events')
-    .setDefaultMemberPermissions(0),
-  execute: async (interaction: CommandInteraction) => {
-    await interaction.deferReply({ ephemeral: false });
-    const calendar = new GCalendarProvider();
-    const events = await calendar.listEvents();
-    let file: AttachmentBuilder | undefined;
-    const reply = commonEmbedBuilder('일정 전체공지').setDescription(
-      '30일 내 예정된 일정 목록입니다.'
-    );
-    if (events.length && events[0].file) {
-      const drive = new GDriveProvider();
-      const dataStr = await drive.getImage(events[0].file);
-      const name = `${events[0].file!}.${dataStr.ext}`;
-      file = new AttachmentBuilder(dataStr.buffer, {
-        name,
-      });
-      reply.setImage(`attachment://${name}`);
-    }
-    events.forEach(event => {
-      reply.addFields({
-        name: `[${event.date}] ${event.title}`,
-        value: `${event.description}`,
-      });
-    });
-    await interaction.editReply({
-      embeds: [reply],
-      files: file ? [file] : [],
-    });
-  },
-};
 export const openWebView: SlashCommand = {
   data: new SlashCommandBuilder()
     .setName('cal-open')
-    .setDescription('open calender web view'),
+    .setNameLocalizations({ko: '일정열기'})
+    .setDescription('open calender web view')
+    .setDescriptionLocalizations({ko: '캘린더 뷰어를 웹으로 보여줍니다'}),
   execute: async (interaction: CommandInteraction) => {
     const reply = commonEmbedBuilder('캘린더 바로가기').setDescription(
       '아래 버튼을 누르면 캘린더 웹페이지로 이동합니다.'
@@ -155,7 +86,8 @@ export const openWebView: SlashCommand = {
 export const subscribe: SlashCommand = {
   data: new SlashCommandBuilder()
     .setName('cal-subscribe')
-    .setDescription('open subscription link'),
+    .setNameLocalizations({ko: '일정구독'})
+    .setDescription('open subscription link').setDescriptionLocalizations({ko: '내 구글 캘린더에 코모라 캘린더를 추가하여 구독합니다.'}),
   execute: async (interaction: CommandInteraction) => {
     const reply = commonEmbedBuilder('캘린더 구독하기').setDescription(
       '아래 버튼을 누르면 사용하는 구글 계정에 캘린더를 연동할 수 있습니다 (PC)'
@@ -178,7 +110,9 @@ export const invite: SlashCommand = {
       option.setName('e-mail').setDescription('Your Email').setRequired(true)
     )
     .setName('cal-invite')
-    .setDescription('send invitation email'),
+    .setNameLocalizations({ko: '일정참여예약'})
+    .setDescription('send invitation email')
+    .setDescriptionLocalizations({ko: '이메일을 등록하여 다가올 이벤트에 참여를 예약합니다.'}),
 
   execute: async (interaction: CommandInteraction) => {
     const email = (interaction.options as any).getString('e-mail');
@@ -189,7 +123,7 @@ export const invite: SlashCommand = {
       });
       return;
     }
-    await interaction.deferReply({ ephemeral: true });
+    await interaction.deferReply({ephemeral: true});
     const calendar = new GCalendarProvider();
     const events = await calendar.listEvents();
     let file: AttachmentBuilder | undefined;
@@ -230,3 +164,77 @@ export const invite: SlashCommand = {
     });
   },
 };
+
+// TBD
+// export const Ping: SlashCommand = {
+//   data: new SlashCommandBuilder()
+//     .setName('ping')
+//     .setNameLocalizations({ko: '서버핑'})
+//     .setDescription('server ping')
+//     .setDescriptionLocalizations({ko: '서버 연결을 확인합니다'}),
+//   execute: async (interaction: CommandInteraction) => {
+//     const exampleEmbed = new EmbedBuilder()
+//       .setTitle('Some title')
+//       .setURL('https://discord.js.org/')
+//       .setAuthor({
+//         name: 'Some name',
+//         iconURL: 'https://i.imgur.com/AfFp7pu.png',
+//         url: 'https://discord.js.org',
+//       })
+//       .setDescription('Some description here')
+//       .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+//       .addFields(
+//         {name: 'Regular field title', value: 'Some value here'},
+//         {name: '\u200B', value: '\u200B'},
+//         {name: 'Inline field title', value: 'Some value here', inline: true},
+//         {name: 'Inline field title', value: 'Some value here', inline: true}
+//       )
+//       .addFields({
+//         name: 'Inline field title',
+//         value: 'Some value here',
+//         inline: true,
+//       })
+//       .setImage('https://i.imgur.com/AfFp7pu.png')
+//       .setTimestamp()
+//       .setFooter({
+//         text: 'Some footer text here',
+//         iconURL: 'https://i.imgur.com/AfFp7pu.png',
+//       });
+//     await interaction.reply({embeds: [exampleEmbed], ephemeral: true});
+//   },
+// };
+// export const broadcastEvents: SlashCommand = {
+//   data: new SlashCommandBuilder()
+//     .setName('cal-alert')
+//     .setDescription('alert channel upcoming events')
+//     .setDefaultMemberPermissions(0),
+//   execute: async (interaction: CommandInteraction) => {
+//     await interaction.deferReply({ ephemeral: false });
+//     const calendar = new GCalendarProvider();
+//     const events = await calendar.listEvents();
+//     let file: AttachmentBuilder | undefined;
+//     const reply = commonEmbedBuilder('일정 전체공지').setDescription(
+//       '30일 내 예정된 일정 목록입니다.'
+//     );
+//     if (events.length && events[0].file) {
+//       const drive = new GDriveProvider();
+//       const dataStr = await drive.getImage(events[0].file);
+//       const name = `${events[0].file!}.${dataStr.ext}`;
+//       file = new AttachmentBuilder(dataStr.buffer, {
+//         name,
+//       });
+//       reply.setImage(`attachment://${name}`);
+//     }
+//     events.forEach(event => {
+//       reply.addFields({
+//         name: `[${event.date}] ${event.title}`,
+//         value: `${event.description}`,
+//       });
+//     });
+//     await interaction.editReply({
+//       embeds: [reply],
+//       files: file ? [file] : [],
+//     });
+//   },
+// };
+
